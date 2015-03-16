f4562d6addbe2918492b04a72f62fcceada17e31
Potpourri <pot_pourri@mail.ru>
2015-03-16 11:04:34 +0300

    Add GLSL 1.50 shaders

--- src/shader.rs
+++ src/shader.rs
@@ -10,6 +10,19 @@ void main()
 }
 ";
 
+pub static VS_COLORED_150: &'static str = "
+#version 150 core
+uniform vec4 color;
+
+in vec2 position;
+
+void main()
+{
+    gl_Position = vec4(position, 0.0, 1.0);
+}
+";
+
+
 pub static FS_COLORED_120: &'static str = "
 #version 120
 uniform vec4 color;
@@ -19,6 +32,19 @@ void main()
     gl_FragColor = color;
 }
 ";
+
+pub static FS_COLORED_150: &'static str = "
+#version 150 core
+uniform vec4 color;
+
+out vec4 o_Color;
+
+void main()
+{
+    o_Color = color;
+}
+";
+
 
 pub static VS_TEXTURED_120: &'static str = "
 #version 120
@@ -38,6 +64,24 @@ void main()
 }
 ";
 
+pub static VS_TEXTURED_150: &'static str = "
+#version 150 core
+uniform sampler2D s_texture;
+uniform vec4 color;
+
+in vec2 position;
+in vec2 texcoord;
+
+out vec2 v_texcoord;
+
+void main()
+{
+    v_texcoord = texcoord;
+    gl_Position = vec4(position, 0.0, 1.0);
+}
+";
+
+
 pub static FS_TEXTURED_120: &'static str = "
 #version 120
 uniform vec4 color;
@@ -48,5 +92,20 @@ varying vec2 v_texcoord;
 void main()
 {
     gl_FragColor = texture2D(s_texture, v_texcoord) * color;
+}
+";
+
+pub static FS_TEXTURED_150: &'static str = "
+#version 150 core
+uniform sampler2D s_texture;
+uniform vec4 color;
+
+out vec4 o_Color;
+
+in vec2 v_texcoord;
+
+void main()
+{
+    o_Color = texture(s_texture, v_texcoord) * color;
 }
 ";
