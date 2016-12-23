PistonDevelopers/glium_graphics

commit c9e53ec4ac17c91fd208890b132c81ab323af1ff

Fixed sub-pixel sampling by adding one-pixel margin
- Bumped to 0.32.1

src/glyph_cache.rs
@@ -100,20 +100,21 @@ impl<F: Facade> CharacterCache for GlyphCache<F> {
                         min: Point{x: 0, y: 0},
                         max: Point{x: 0, y: 0}
                     });
-                let pixel_bb_width = pixel_bounding_box.width();
-                let pixel_bb_height = pixel_bounding_box.height();
+                // Add one-pixel margin to fix sampling.
+                let pixel_bb_width = pixel_bounding_box.width() + 2;
+                let pixel_bb_height = pixel_bounding_box.height() + 2;
 
                 let mut image_buffer = Vec::<u8>::new();
                 image_buffer.resize((pixel_bb_width * pixel_bb_height) as usize, 0);
                 glyph.draw(|x, y, v| {
-                   let pos = (x + y * (pixel_bb_width as u32)) as usize;
+                   let pos = ((x+1) + (y+1) * (pixel_bb_width as u32)) as usize;
                    image_buffer[pos] = (255.0 * v) as u8;
                 });
 
                 let &mut (offset, size, ref texture) = v.insert((
                     [
-                        bounding_box.min.x as Scalar,
-                        -pixel_bounding_box.min.y as Scalar,
+                        bounding_box.min.x as Scalar + 1.0,
+                        -pixel_bounding_box.min.y as Scalar + 1.0,
                     ],
                     [
                         h_metrics.advance_width as Scalar,

Cargo.toml
@@ -1,6 +1,6 @@
 [package]
 name = "piston2d-glium_graphics"
-version = "0.32.0"
+version = "0.32.1"
 authors = [
     "Eduard Bopp <eduard.bopp@aepsil0n.de>",
     "bvssvni <bvssvni@gmail.com>"

commit bffe2cfc2aebec10a5f0827adf11ef88a1df610c

Fix bug causing text to be drawn 2 pixels off
It appears that c9e53ec4ac17c91fd208890b132c81ab323af1ff introduced a bug
causing text to be drawn 2 pixels to the right of where it should be.

src/glyph_cache.rs
@@ -113,7 +113,7 @@ impl<F: Facade> CharacterCache for GlyphCache<F> {
 
                 let &mut (offset, size, ref texture) = v.insert((
                     [
-                        bounding_box.min.x as Scalar + 1.0,
+                        bounding_box.min.x as Scalar - 1.0,
                         -pixel_bounding_box.min.y as Scalar + 1.0,
                     ],
                     [